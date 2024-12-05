283bacdf9e8eb81ad1112516681801c301158dd4
naopiiii <nmam44nao@icloud.com>
2024-12-05 10:04:14 +0800

    toppage
    担当を振り分けました

---
+++ resources/views/posts/top.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>toppage|trippy</title>
+    <link rel="stylesheet" href="{{ asset('/css/tops.css') }}">
+</head>
+<body>
+{{-- ヘッダーcana --}}
+
+{{-- 検索ayaka --}}
+
+{{-- タグayaka --}}
+
+{{-- 記事一覧nao --}}
+
+{{-- フッダーcana --}}
+
+</body>
+</html>
