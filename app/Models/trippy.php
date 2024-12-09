1a928a7fbf636f4593dad2710c9cb75259ddf29e
a-yaka <otayk.hiamoe@outlook.jp>
2024-12-09 12:43:31 +0800

    Models修正

--- app/Models/trippy.php
+++ app/Models/trippy.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class trippy extends Model
+class Trippy extends Model
 {
     use HasFactory;
 }
