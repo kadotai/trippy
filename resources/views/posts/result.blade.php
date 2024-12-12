91205dd072cc68f5797eae74ddfb3feca84bcd10
yamadaxxx09 <152535603+yamadaxxx09@users.noreply.github.com>
2024-12-11 19:27:30 -0700

    かなさんのところ確認

--- resources/views/posts/result.blade.php
+++ resources/views/posts/result.blade.php
@@ -1,39 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>result|trippy</title>
-</head>
-<body>
 
-    {{-- 検索欄 --}}
-    <section class="result_search">
-        <div class="search">
-              <input type="search" id="post-search" class="search_box">
-              <button class="search_button">検索</button>
-        </div>
-    </section>
-    
-    {{-- タグ --}}
-    <section class="result_selected_tag">
-        @foreach ($tags as $tag)
-          <p class="tag">{{ $tag -> tag_name }}</p>
-        @endforeach
-    </section>
-
-    {{-- 検索結果 --}}
-    <section>
-
-    </section>
-
-</body>
-</html>
-@extends('layouts.original')
-@section('css')
 <link rel="stylesheet" href="{{ asset('assets/css/top.css') }}">
-@endsection
+{{-- @endsection --}}
 
 @section('content')
 {{-- @foreach($posts as $post) --}}
@@ -116,4 +83,4 @@
         </div>
     </a></div>
 </section>
-@endsection
+{{-- @endsection --}}
